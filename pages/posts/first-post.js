256f7d2826594c74ec8e07f2335e57b92e0625f2
misasa <2180266@ecccomp.com>
2020-09-29 17:42:07 +0900

    Lesson3 Layoutコンポーネント

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,12 +1,13 @@
 import Link from 'next/link'
 import Head from 'next/head'
+import Layout from '../../components/layout'
 
 export default function FirstPost() {
   return (
-    <>
-    <Head>
-        <title>First Post</title>
-    </Head>
+    <Layout>
+      <Head>
+          <title>First Post</title>
+      </Head>
     
       <h1>First Post</h1>
       <h2>
@@ -14,6 +15,6 @@ export default function FirstPost() {
           <a>Back to home</a>
         </Link>
       </h2>
-    </>
+    </Layout>
   )
 }
