f1b717c620f3490dbbcb0bcfe9e4a93f703bbf22
嶋村星哉 <>
2020-10-09 15:36:13 +0900

    Add レッスン７前の状態

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -5,37 +5,35 @@ import Date from '../../components/date'
 import utilStyles from '../../styles/utils.module.css'
 
 export default function Post({ postData }) {
-    return (
-        <Layout>
-            <Head>
-                <title>{postData.title}</title>
-            </Head>
-            <article>
-                <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-                <div className={utilStyles.lightText}>
-                    <Date dateString={postData.date} />
-                </div>
-                <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-            </article>
-        </Layout>
-    )
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  )
 }
 
 export async function getStaticPaths() {
-    // id としてとりうる値のリストを返す
-    const paths = getAllPostIds()
-    return {
-        paths,
-        fallback: false
-    }
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false
+  }
 }
 
 export async function getStaticProps({ params }) {
-    // params.id を使用して、ブログの投稿に必要なデータを取得する
-    const postData = await getPostData(params.id)
-    return {
-        props: {
-            postData
-        }
+  const postData = await getPostData(params.id)
+  return {
+    props: {
+      postData
     }
+  }
 }
